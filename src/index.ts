23505d4c108388e983a021a3bd45bd7a77f97300
Aaron-Earley <aaronearley@hotmail.com>
2025-04-22 15:36:38 +0100

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,12 @@
-import {StripeConstructor} from '../types';
-import {loadScript, initStripe, LoadStripe} from './shared';
-
-let stripePromise: Promise<StripeConstructor | null> | null;
-let loadCalled = false;
-
-const getStripePromise: () => Promise<StripeConstructor | null> = () => {
-  if (stripePromise) {
-    return stripePromise;
-  }
-
-  stripePromise = loadScript(null).catch((error) => {
-    // clear cache on error
-    stripePromise = null;
-    return Promise.reject(error);
-  });
-  return stripePromise;
-};
-
-// Execute our own script injection after a tick to give users time to do their
-// own script injection.
-Promise.resolve()
-  .then(() => getStripePromise())
-  .catch((error) => {
-    if (!loadCalled) {
-      console.warn(error);
-    }
-  });
-
-export const loadStripe: LoadStripe = (...args) => {
-  loadCalled = true;
-  const startTime = Date.now();
-
-  // if previous attempts are unsuccessful, will re-load script
-  return getStripePromise().then((maybeStripe) =>
-    initStripe(maybeStripe, args, startTime)
-  );
-};
+import GoTrueAdminApi from './GoTrueAdminApi'
+import GoTrueClient from './GoTrueClient'
+import AuthAdminApi from './AuthAdminApi'
+import AuthClient from './AuthClient'
+export { GoTrueAdminApi, GoTrueClient, AuthAdminApi, AuthClient }
+export * from './lib/types'
+export * from './lib/errors'
+export {
+  navigatorLock,
+  NavigatorLockAcquireTimeoutError,
+  internals as lockInternals,
+} from './lib/locks'
